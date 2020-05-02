7590aba195f8561f4478e9d4feca1ce6ef6d88e1
RoozbehBandpey <r.bandpey67@gmail.com>
2020-05-02 22:22:29 +0200

    usings

--- OCRVisionApp/OCRVision.cs
+++ OCRVisionApp/OCRVision.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace OCRVisionApp
 {
