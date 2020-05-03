d5def42616e5b949d4589187951f5aabccf23da5
RoozbehBandpey <r.bandpey67@gmail.com>
2020-05-03 15:08:00 +0200

    dasving image metadata

--- OCRVisionApp/OCRVision.cs
+++ OCRVisionApp/OCRVision.cs
@@ -19,6 +19,143 @@ namespace OCRVisionApp
             return client;
         }
 
+        private static void SaveJson(ImageAnalysis imageMetadata)
+        {
+            var jsonResults = JsonConvert.SerializeObject(imageMetadata);
+            Console.WriteLine(imageMetadata);
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectBinDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+            string projectDirectory = Directory.GetParent(projectBinDirectory).FullName;
+            string MetadataPath = Path.Combine(projectDirectory, "ImageMetadata");
+            if (!Directory.Exists(MetadataPath))
+                Directory.CreateDirectory(MetadataPath);
+        }
+
+
+        private static void ShowResults(ImageAnalysis imageMetadata)
+        {
+            // Sunmarizes the image content.
+            Console.WriteLine("Summary:");
+            foreach (var caption in imageMetadata.Description.Captions)
+            {
+                Console.WriteLine($"{caption.Text} with confidence {caption.Confidence}");
+            }
+            Console.WriteLine();
+
+            // Display categories the image is divided into.
+            Console.WriteLine("Categories:");
+            foreach (var category in imageMetadata.Categories)
+            {
+                Console.WriteLine($"{category.Name} with confidence {category.Score}");
+            }
+            Console.WriteLine();
+            // </snippet_categorize>
+
+            // <snippet_tags>
+            // Image tags and their confidence score
+            Console.WriteLine("Tags:");
+            foreach (var tag in imageMetadata.Tags)
+            {
+                Console.WriteLine($"{tag.Name} {tag.Confidence}");
+            }
+            Console.WriteLine();
+            // </snippet_tags>
+
+            // <snippet_objects>
+            // Objects
+            Console.WriteLine("Objects:");
+            foreach (var obj in imageMetadata.Objects)
+            {
+                Console.WriteLine($"{obj.ObjectProperty} with confidence {obj.Confidence} at location {obj.Rectangle.X}, " +
+                  $"{obj.Rectangle.X + obj.Rectangle.W}, {obj.Rectangle.Y}, {obj.Rectangle.Y + obj.Rectangle.H}");
+            }
+            Console.WriteLine();
+            // </snippet_objects>
+
+            // <snippet_faces>
+            // Faces
+            Console.WriteLine("Faces:");
+            foreach (var face in imageMetadata.Faces)
+            {
+                Console.WriteLine($"A {face.Gender} of age {face.Age} at location {face.FaceRectangle.Left}, " +
+                  $"{face.FaceRectangle.Left}, {face.FaceRectangle.Top + face.FaceRectangle.Width}, " +
+                  $"{face.FaceRectangle.Top + face.FaceRectangle.Height}");
+            }
+            Console.WriteLine();
+            // </snippet_faces>
+
+            // <snippet_adult>
+            // Adult or racy content, if any.
+            Console.WriteLine("Adult:");
+            Console.WriteLine($"Has adult content: {imageMetadata.Adult.IsAdultContent} with confidence {imageMetadata.Adult.AdultScore}");
+            Console.WriteLine($"Has racy content: {imageMetadata.Adult.IsRacyContent} with confidence {imageMetadata.Adult.RacyScore}");
+            Console.WriteLine();
+            // </snippet_adult>
+
+            // <snippet_brands>
+            // Well-known (or custom, if set) brands.
+            Console.WriteLine("Brands:");
+            foreach (var brand in imageMetadata.Brands)
+            {
+                Console.WriteLine($"Logo of {brand.Name} with confidence {brand.Confidence} at location {brand.Rectangle.X}, " +
+                  $"{brand.Rectangle.X + brand.Rectangle.W}, {brand.Rectangle.Y}, {brand.Rectangle.Y + brand.Rectangle.H}");
+            }
+            Console.WriteLine();
+            // </snippet_brands>
+
+            // <snippet_celebs>
+            // Celebrities in image, if any.
+            Console.WriteLine("Celebrities:");
+            foreach (var category in imageMetadata.Categories)
+            {
+                if (category.Detail?.Celebrities != null)
+                {
+                    foreach (var celeb in category.Detail.Celebrities)
+                    {
+                        Console.WriteLine($"{celeb.Name} with confidence {celeb.Confidence} at location {celeb.FaceRectangle.Left}, " +
+                          $"{celeb.FaceRectangle.Top}, {celeb.FaceRectangle.Height}, {celeb.FaceRectangle.Width}");
+                    }
+                }
+            }
+            Console.WriteLine();
+            // </snippet_celebs>
+
+
+            // <snippet_landmarks>
+            // Popular landmarks in image, if any.
+            Console.WriteLine("Landmarks:");
+            foreach (var category in imageMetadata.Categories)
+            {
+                if (category.Detail?.Landmarks != null)
+                {
+                    foreach (var landmark in category.Detail.Landmarks)
+                    {
+                        Console.WriteLine($"{landmark.Name} with confidence {landmark.Confidence}");
+                    }
+                }
+            }
+            Console.WriteLine();
+            // </snippet_landmarks>
+
+            // <snippet_color>
+            // Identifies the color scheme.
+            Console.WriteLine("Color Scheme:");
+            Console.WriteLine("Is black and white?: " + imageMetadata.Color.IsBWImg);
+            Console.WriteLine("Accent color: " + imageMetadata.Color.AccentColor);
+            Console.WriteLine("Dominant background color: " + imageMetadata.Color.DominantColorBackground);
+            Console.WriteLine("Dominant foreground color: " + imageMetadata.Color.DominantColorForeground);
+            Console.WriteLine("Dominant colors: " + string.Join(",", imageMetadata.Color.DominantColors));
+            Console.WriteLine();
+            // </snippet_color>
+
+            // <snippet_type>
+            // Detects the image types.
+            Console.WriteLine("Image Type:");
+            Console.WriteLine("Clip Art Type: " + imageMetadata.ImageType.ClipArtType);
+            Console.WriteLine("Line Drawing Type: " + imageMetadata.ImageType.LineDrawingType);
+            Console.WriteLine();
+        }
+
         public static async Task AnalyzeImageWithUrl(ComputerVisionClient client, string imageUrl)
         {
             Console.WriteLine("----------------------------------------------------------");
@@ -40,126 +177,11 @@ namespace OCRVisionApp
             // Analyze the URL image 
             ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, features);
 
-            // Sunmarizes the image content.
-            Console.WriteLine("Summary:");
-            foreach (var caption in results.Description.Captions)
-            {
-                Console.WriteLine($"{caption.Text} with confidence {caption.Confidence}");
-            }
-            Console.WriteLine();
+            //ShowResults(results);
 
-            // Display categories the image is divided into.
-            Console.WriteLine("Categories:");
-            foreach (var category in results.Categories)
-            {
-                Console.WriteLine($"{category.Name} with confidence {category.Score}");
-            }
-            Console.WriteLine();
-            // </snippet_categorize>
+            SaveJson(results);
 
-            // <snippet_tags>
-            // Image tags and their confidence score
-            Console.WriteLine("Tags:");
-            foreach (var tag in results.Tags)
-            {
-                Console.WriteLine($"{tag.Name} {tag.Confidence}");
-            }
-            Console.WriteLine();
-            // </snippet_tags>
-
-            // <snippet_objects>
-            // Objects
-            Console.WriteLine("Objects:");
-            foreach (var obj in results.Objects)
-            {
-                Console.WriteLine($"{obj.ObjectProperty} with confidence {obj.Confidence} at location {obj.Rectangle.X}, " +
-                  $"{obj.Rectangle.X + obj.Rectangle.W}, {obj.Rectangle.Y}, {obj.Rectangle.Y + obj.Rectangle.H}");
-            }
-            Console.WriteLine();
-            // </snippet_objects>
-
-            // <snippet_faces>
-            // Faces
-            Console.WriteLine("Faces:");
-            foreach (var face in results.Faces)
-            {
-                Console.WriteLine($"A {face.Gender} of age {face.Age} at location {face.FaceRectangle.Left}, " +
-                  $"{face.FaceRectangle.Left}, {face.FaceRectangle.Top + face.FaceRectangle.Width}, " +
-                  $"{face.FaceRectangle.Top + face.FaceRectangle.Height}");
-            }
-            Console.WriteLine();
-            // </snippet_faces>
-
-            // <snippet_adult>
-            // Adult or racy content, if any.
-            Console.WriteLine("Adult:");
-            Console.WriteLine($"Has adult content: {results.Adult.IsAdultContent} with confidence {results.Adult.AdultScore}");
-            Console.WriteLine($"Has racy content: {results.Adult.IsRacyContent} with confidence {results.Adult.RacyScore}");
-            Console.WriteLine();
-            // </snippet_adult>
-
-            // <snippet_brands>
-            // Well-known (or custom, if set) brands.
-            Console.WriteLine("Brands:");
-            foreach (var brand in results.Brands)
-            {
-                Console.WriteLine($"Logo of {brand.Name} with confidence {brand.Confidence} at location {brand.Rectangle.X}, " +
-                  $"{brand.Rectangle.X + brand.Rectangle.W}, {brand.Rectangle.Y}, {brand.Rectangle.Y + brand.Rectangle.H}");
-            }
-            Console.WriteLine();
-            // </snippet_brands>
-
-            // <snippet_celebs>
-            // Celebrities in image, if any.
-            Console.WriteLine("Celebrities:");
-            foreach (var category in results.Categories)
-            {
-                if (category.Detail?.Celebrities != null)
-                {
-                    foreach (var celeb in category.Detail.Celebrities)
-                    {
-                        Console.WriteLine($"{celeb.Name} with confidence {celeb.Confidence} at location {celeb.FaceRectangle.Left}, " +
-                          $"{celeb.FaceRectangle.Top}, {celeb.FaceRectangle.Height}, {celeb.FaceRectangle.Width}");
-                    }
-                }
-            }
-            Console.WriteLine();
-            // </snippet_celebs>
-
-
-            // <snippet_landmarks>
-            // Popular landmarks in image, if any.
-            Console.WriteLine("Landmarks:");
-            foreach (var category in results.Categories)
-            {
-                if (category.Detail?.Landmarks != null)
-                {
-                    foreach (var landmark in category.Detail.Landmarks)
-                    {
-                        Console.WriteLine($"{landmark.Name} with confidence {landmark.Confidence}");
-                    }
-                }
-            }
-            Console.WriteLine();
-            // </snippet_landmarks>
-
-            // <snippet_color>
-            // Identifies the color scheme.
-            Console.WriteLine("Color Scheme:");
-            Console.WriteLine("Is black and white?: " + results.Color.IsBWImg);
-            Console.WriteLine("Accent color: " + results.Color.AccentColor);
-            Console.WriteLine("Dominant background color: " + results.Color.DominantColorBackground);
-            Console.WriteLine("Dominant foreground color: " + results.Color.DominantColorForeground);
-            Console.WriteLine("Dominant colors: " + string.Join(",", results.Color.DominantColors));
-            Console.WriteLine();
-            // </snippet_color>
-
-            // <snippet_type>
-            // Detects the image types.
-            Console.WriteLine("Image Type:");
-            Console.WriteLine("Clip Art Type: " + results.ImageType.ClipArtType);
-            Console.WriteLine("Line Drawing Type: " + results.ImageType.LineDrawingType);
-            Console.WriteLine();
+           
         }
     }
 }
