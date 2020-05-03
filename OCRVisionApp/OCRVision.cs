34aae20cf8dca988b980ca2128460929f97788dc
RoozbehBandpey <r.bandpey67@gmail.com>
2020-05-03 12:16:05 +0200

    anlayzing image

--- OCRVisionApp/OCRVision.cs
+++ OCRVisionApp/OCRVision.cs
@@ -18,5 +18,37 @@ namespace OCRVisionApp
               { Endpoint = endpoint };
             return client;
         }
+
+        public static async Task AnalyzeImageWithUrl(ComputerVisionClient client, string imageUrl)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("ANALYZE IMAGE - URL");
+            Console.WriteLine();
+
+            // Creating a list that defines the features to be extracted from the image. 
+            List<VisualFeatureTypes> features = new List<VisualFeatureTypes>()
+            {
+                  VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+                  VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+                  VisualFeatureTypes.Tags, VisualFeatureTypes.Adult,
+                  VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
+                  VisualFeatureTypes.Objects
+            };
+
+            Console.WriteLine($"Analyzing the image {Path.GetFileName(imageUrl)}...");
+            Console.WriteLine();
+            // Analyze the URL image 
+            ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, features);
+
+            // Sunmarizes the image content.
+            Console.WriteLine("Summary:");
+            foreach (var caption in results.Description.Captions)
+            {
+                Console.WriteLine($"{caption.Text} with confidence {caption.Confidence}");
+            }
+            Console.WriteLine();
+
+            //return results;
+        }
     }
 }
--- OCRVisionApp/Program.cs
+++ OCRVisionApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using OCRVisionApp;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 
 namespace OCRVisionApp
 {
@@ -16,9 +17,13 @@ namespace OCRVisionApp
             Console.WriteLine(subscriptionName);
             Console.WriteLine(endpoint);
             Console.WriteLine(key);
-            ComputerVisionClient client = OCRVisionApp.OCRVision.Authenticate(endpoint, key);
+            ComputerVisionClient client = OCRVision.Authenticate(endpoint, key);
 
             Console.WriteLine(client);
+
+            string ANALYZE_URL_IMAGE = "https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png";
+
+            OCRVision.AnalyzeImageWithUrl(client, ANALYZE_URL_IMAGE);
         }
     }
 }
